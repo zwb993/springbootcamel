696830f17bda4e407b707ac39f141b9d90d9731a
zhengweibing3 <zhengweibing3@jd.com>
2021-04-21 11:21:06 +0800

    init

---
+++ src/main/java/com/zwb/SpringbootcamelApplication.java
@@ -0,0 +1,13 @@
+package com.zwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcamelApplication.class, args);
+	}
+
+}
